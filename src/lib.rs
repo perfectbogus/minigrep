2aec91b4e76d118d9b62cf8b1f513dba58ca96d8
eduardo escobar <war_army_10@hotmail.com>
2024-10-07 21:12:53 -0600

    Using iterator Trait Methods instead of indexing

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,20 @@ pub struct Config {
 impl Config {
 
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        args.next();// first value is the name of the program executed.
+
+        let query = match args.next() {
+            None => { return Err("Didn't get a query string") },
+            Some(arg) =>  arg ,
+        };
+
+        let file_path = match args.next() {
+            None => { return Err("Didn't get a file path") },
+            Some(arg) =>  arg,
+        };
+        
         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config { query, file_path, ignore_case })
     }
 }
